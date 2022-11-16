c679991407f29d833a88a6a3bd86a039bea842a6
Sahar11 <saharhalim151@gmail.com>
2022-11-16 11:19:07 -0500

    add tableViewController

--- food_journal/Controllers/HomeViewController.swift
+++ food_journal/Controllers/HomeViewController.swift
@@ -7,18 +7,36 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+class HomeViewController: UITableViewController{
 
     @IBOutlet weak var search_text: UITextField!
+    
+    let itemArray = ["Buy Butter", "Buy Eggs", "Go to hogwarts"]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-    @IBAction func pressSearch(_ sender: UIButton) {
+    //MARK - TableView
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
     }
     
-    @IBAction func pressToTakePicture(_ sender: UIButton) {
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ImageItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+    
+    //MARK - TableView Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(itemArray[indexPath.row])
+        
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 }
 
